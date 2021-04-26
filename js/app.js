56817b9a5295aadd0f269a60f814ba74d595d5e7
Momansi96 <mansimohammed8@gmail.com>
2021-04-27 02:06:31 +0300

    finished the survey

--- js/app.js
+++ js/app.js
@@ -5,30 +5,97 @@ alert('Hi ' + userName + ' and welcome to this small quiz.')
 
 let devGender = prompt('Do you think I am male?').toLowerCase();
 if (devGender == 'yes' || devGender == 'y') {
-    console.log('You are correct');
-    
+    // console.log('You are correct');
+    alert('You are correct'); 
 }
  else if (devGender == 'no' || devGender == 'n') {
-    console.log('Unfortunately you are wrong');
+    // console.log('Unfortunately you are wrong');
+    alert('Unfortunately you are wrong'); 
     } 
   else {
-   console.log('Please enter yes or no'); 
+//    console.log('Please enter yes or no'); 
+    alert('Please enter only yes or no'); 
   
   }
 
   
 let devEdu = prompt('Do you think I have a masters degree?').toLowerCase();
   if (devEdu == 'yes' || devEdu == 'y') {
-      console.log('Unfortunately You are wrong');
+    //   console.log('Unfortunately You are wrong');
+    alert('Unfortunately you are wrong');
       
   }
    else if (devEdu == 'no' || devEdu == 'n') {
-      console.log('you are correct');
+    //   console.log('you are correct');
+      alert('You are correct'); 
       } 
     else {
-     console.log('Please enter yes or no'); 
+    //  console.log('Please enter only yes or no');
+    alert('Please enter only yes or no'); 
     
     }
+
+    let devExp = prompt('Do you think I have previous experince in programming?').toLowerCase();
+if (devExp == 'yes' || devExp == 'y') {
+    // console.log('You are correct');
+    alert('Unfortunately you are wrong');
+    
+}
+ else if (devExp == 'no' || devExp == 'n') {
+    // console.log('Unfortunately you are wrong');
+    alert('You are correct'); 
+    } 
+  else {
+//    console.log('Please enter yes or no'); 
+    alert('Please enter only yes or no'); 
+  
+  }
+
+  let devHob = prompt('Do you think I practice sports?').toLowerCase();
+  if (devHob == 'yes' || devHob == 'y') {
+      // console.log('You are correct');
+      alert('Unfortunately you are wrong');
+      
+  }
+   else if (devHob == 'no' || devHob == 'n') {
+      // console.log('Unfortunately you are wrong');
+      alert('You are correct'); 
+      } 
+    else {
+  //    console.log('Please enter yes or no'); 
+      alert('Please enter only yes or no'); 
+    
+    }
+  
+    let devWatch = prompt('Do you think I watch sports?').toLowerCase();
+    if (devWatch == 'yes' || devWatch == 'y') {
+        // console.log('You are correct');
+        alert('You are correct'); 
+    }
+     else if (devWatch == 'no' || devWatch == 'n') {
+        // console.log('Unfortunately you are wrong');
+        alert('Unfortunately you are wrong'); 
+        } 
+      else {
+    //    console.log('Please enter yes or no'); 
+        alert('Please enter only yes or no'); 
+      
+      }
+
+      let devEnjoy = prompt('Do you think I am enjoying this course?').toLowerCase();
+      if (devEnjoy == 'yes' || devEnjoy == 'y') {
+          // console.log('You are correct');
+          alert('You are correct'); 
+      }
+       else if (devEnjoy == 'no' || devEnjoy == 'n') {
+          // console.log('Unfortunately you are wrong');
+          alert('Unfortunately you are wrong'); 
+          } 
+        else {
+      //    console.log('Please enter yes or no'); 
+          alert('Please enter only yes or no'); 
+        
+        }
 
 
 
